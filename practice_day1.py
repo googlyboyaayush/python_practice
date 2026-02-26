38587023ea5ec0e85787e82c4933b2899d3f8eee
googlyboyaayush <aayushsingh6284@gmail.com>
2026-02-26 18:06:21 +0530

    practice_day_1

---
+++ practice_day1.py
@@ -0,0 +1,17 @@
+# name = "aayush"
+# print ("Hello",name)
+
+# num1= int(input())
+# num2= int(input())
+# sum = num1+num2
+# print("sum","=",sum)   
+
+# name = (input("Enter name:"))
+# place = (input("Enter you place:"))
+# print("Hello",name)
+# print("You are from",place)
+
+num= int(input())
+if num==2:
+    print("Its a even")
+    
